bd50e1e0c4043ba5b3e66e0ad302b87973ccccc3
LanceMaverick <pnoel6@gmail.com>
2016-10-18 13:52:44 +0200

    first commit

---
+++ extractdata.py
@@ -0,0 +1,14 @@
+import tweepy
+import yaml
+
+
+#TODO: function for extracting training data from yamls
+#TODO: function for extracting training data from .csv (need to know formatting)
+#TODO: tweet to predictor parser
+
+#def prepare_test_data():
+#    """test function to prepare movie review data.
+#    Uses dataset from http://ai.stanford.edu/~amaas/data/sentiment/
+#    for testing until tweet dataset is large enough"""
+    
+
